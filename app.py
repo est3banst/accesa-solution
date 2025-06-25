fdd1a3330d30d2bd07e829e7c367c05f5ea900e9
est3banst <estebeeen@outlook.com>
2025-06-25 16:45:16 -0300

    generate report accesa-data :v2.15:

--- app.py
+++ app.py
@@ -42,6 +42,14 @@ def fetch_monthly_automatismo(table_name):
     """
     return bq.query(query).to_dataframe()
 
+def fetch_monthly_congestion(table_name):
+    bq = bigquery.Client()
+    query = f"""
+        SELECT 
+        * FROM `accesa-equipo3.accesa_dataset.{table_name}` 
+        ;
+    """
+
 def fetch_monthly_reclamos(table_name):
     bq = bigquery.Client()
     query = f"""
@@ -51,6 +59,13 @@ def fetch_monthly_reclamos(table_name):
     """
     return bq.query(query).to_dataframe()
 
+def fetch_monthly_incidencias(table_name):
+    bq = bigquery.Client()
+    query = f"""
+        SELECT 
+        * FROM `accesa-equipo3.accesa_dataset.{table_name}`
+        ;
+    """
 
 def fetch_monthly_habilidad(table_name):
     bq = bigquery.Client()
@@ -83,8 +98,7 @@ def summarize_dataframe(df, context=""):
     model = TextGenerationModel.from_pretrained("text-bison@002")
     prompt = f"""
 Actúa como un analista de datos senior. Resume los siguientes datos ({context}) en español con una narrativa profesional, señalando tendencias, anomalías o valores atípicos.
-
-{df.describe(include='all').to_string()}
+ {df.describe(include='all').to_string()}
 """
     return model.predict(prompt=prompt, temperature=0.7, max_output_tokens=1024).text
 
@@ -201,92 +215,6 @@ def pubsub_hook():
         logger.error(traceback.format_exc())
         return "Internal Error", 500
 
-def calc_skill(df):
-    trsac_antel_movil = df["demora_en_atender"][1] if len(df["demora_en_atender"]) > 1 else 0
-    antel_movil_ofrecidas = df["ofrecidas"][1] if len(df["ofrecidas"]) > 1 else 0
-    referentes_movil_ofrecidas = df["ofrecidas"][0] if len(df["ofrecidas"]) > 0 else 0
-    horas_operacion = df["horas_operacion"][1] if len(df["horas_operacion"]) > 1 else 0
-    promedio_tiempo_operacion_segundos = df["tiempo_operacion"][1] if len(df["tiempo_operacion"]) > 1 else 0
-    calidad = df["calidad_"][1] if len(df["calidad_"]) > 1 else 0
-    nivel_de_servicio = round(calidad * 100 / 80, 2)
-
-    return {
-        "trsac_antel_movil": trsac_antel_movil,
-        "antel_movil_ofrecidas": antel_movil_ofrecidas,
-        "referentes_movil_ofrecidas": referentes_movil_ofrecidas,
-        "horas_operacion": horas_operacion,
-        "nivel_de_servicio": nivel_de_servicio,
-        "promedio_tiempo_operacion_segundos": promedio_tiempo_operacion_segundos,}
-
-def calc_roaming(df):
-    mensajes_entrantes = df["cantidad_de_mensajes_entrantes"]
-    mensajes_salientes = df["cantidad_de_mensajes_salientes"]
-    total_mensajes = mensajes_entrantes + mensajes_salientes
-    promedio_mensajes_por_interaccion = df["promedio_de_mensajes_por_interaccion"]
-
-    return {
-        "mensajes_entrantes": mensajes_entrantes,
-        "mensajes_salientes": mensajes_salientes,
-        "total_mensajes": total_mensajes,
-        "promedio_mensajes_por_interaccion": promedio_mensajes_por_interaccion,
-    }
-
-def calc_automatismo(df):
-    total_correcto = df["total_correcto"].sum()
-    total_error = df["total_error"].sum()
-
-    porcentaje_correcto = ((total_correcto * 100 )/ (total_correcto + total_error)) if (total_correcto + total_error) > 0 else 0
-    porcentaje_error = ((total_error * 100) / (total_correcto + total_error)) if (total_correcto + total_error) > 0 else 0
-    total_automatismos = total_correcto + total_error
-
-    return {
-        "total_correcto": total_correcto,
-        "total_error": total_error,
-        "total_automatismos": total_automatismos,
-        "porcentaje_correcto": round(porcentaje_correcto, 2),
-        "porcentaje_error": round(porcentaje_error, 2),
-    }
-
-
-def calc_habilidad(df):
-    total_llamadas = df["ofrecidas"].sum()
-    atendidas = df["atendidas"].sum()
-    abandonadas = df["abandonadas"].sum()
-    dias_mes = df["fecha"].nunique()
-
-    porcentaje_no_atendidas = (abandonadas / total_llamadas) * 100 if total_llamadas else 0
-    indice_respuesta = 100 - porcentaje_no_atendidas
-    promedio_llamadas = total_llamadas / dias_mes if dias_mes else 0
-
-    return {
-        "llamadas_al_servicio": total_llamadas,
-        "llamadas_atendidas": atendidas,
-        "llamadas_abandonadas": abandonadas,
-        "porcentaje_no_atendidas": round(porcentaje_no_atendidas, 2),
-        "indice_respuesta": round(indice_respuesta, 2),
-        "promedio_llamadas_por_dia": math.trunc(promedio_llamadas),
-    }
-
-def ms_to_hms(ms):
-    seconds = int(ms / 1000)
-    return str(timedelta(seconds=seconds))
-
-def calc_reclamos(df):
-    df["_acumulado_o_detallado_"] = df["_acumulado_o_detallado_"].str.lower()
-    df = df[df["_acumulado_o_detallado_"] == "detallado"]
-
-    total_tiempo_llamadas = df["_manejo_total_"].sum()
-    total_llamadas = df["_manejo_"].sum()
-    nombre_de_cola = df["_nombre_de_cola_"].to_list()
-    nombre_de_codigo_de_conclusion = df["_nombre_de_codigo_de_conclusion_"].to_list()
-
-    return {
-        "total_tiempo_llamadas": ms_to_hms(total_tiempo_llamadas),
-        "total_llamadas": total_llamadas,
-        "nombre_de_cola": nombre_de_cola,
-        "nombre_de_codigo_de_conclusion": nombre_de_codigo_de_conclusion,
-    }
-
 @app.route("/generate-report", methods=["POST"])
 def generate_report():
     handler_map = {
@@ -294,7 +222,9 @@ def generate_report():
     "habilidad_data": (fetch_monthly_habilidad, calc_habilidad),
     "skill_data": (fetch_monthly_skill, calc_skill),
     "reclamos_data":(fetch_monthly_reclamos, calc_reclamos),
+    "congestion_data": (fetch_monthly_congestion, calc_congestion),
     "automatismo_data": (fetch_monthly_automatismo, calc_automatismo),
+    "incidencias_data" : (fetch_monthly_incidencias, calc_incidencias)
     }
     try:
         data = request.get_json()
@@ -329,7 +259,12 @@ def generate_report():
             expiration=timedelta(minutes=15),
             method="GET"
         )
-
+        project_id = "accesa-equipo3"
+        dataset_id = f"{project_id}.accesa_dataset"
+        bq = bigquery.Client()
+        bq.delete_dataset(
+            dataset_id, delete_contents=True, not_found_ok=True
+        )
         return jsonify({"download_url": url}), 200
 
     except Exception as e:
@@ -402,6 +337,8 @@ def process_file(file_name):
                     df = pd.read_excel(io.BytesIO(file_data), skiprows=4)
                 elif "habilidad" in file_name.lower():
                     df = pd.read_excel(io.BytesIO(file_data), skiprows=1)
+                elif "congestion" in file_name.lower():
+                    df = [pd.read_excel(io.BytesIO(file_data), skiprows=4)]
                 else:
                     df = pd.read_excel(io.BytesIO(file_data))
             
@@ -418,6 +355,8 @@ def process_file(file_name):
             table_name = "roaming_data"
         elif "automatismo" in file_name:
             table_name = "automatismo_data"
+        elif "incidencias" in file_name:
+            table_name = "incidencias_data"
         elif "congestion" in file_name:
             table_name = "congestion_data"
         elif "habilidad" in file_name:
@@ -454,6 +393,113 @@ def process_file(file_name):
         logger.error(f"Error in process_upload: {str(e)}")
         logger.error(traceback.format_exc())
         raise
+
+
+def calc_skill(df):
+    trsac_antel_movil = df["demora_en_atender"][1] if len(df["demora_en_atender"]) > 1 else 0
+    antel_movil_ofrecidas = df["ofrecidas"][1] if len(df["ofrecidas"]) > 1 else 0
+    referentes_movil_ofrecidas = df["ofrecidas"][0] if len(df["ofrecidas"]) > 0 else 0
+    horas_operacion = df["horas_operacion"][1] if len(df["horas_operacion"]) > 1 else 0
+    promedio_tiempo_operacion_segundos = df["tiempo_operacion"][1] if len(df["tiempo_operacion"]) > 1 else 0
+    calidad = df["calidad_"][1] if len(df["calidad_"]) > 1 else 0
+    nivel_de_servicio = round(calidad * 100 / 80, 2)
+
+    return {
+        "trsac_antel_movil": trsac_antel_movil,
+        "antel_movil_ofrecidas": antel_movil_ofrecidas,
+        "referentes_movil_ofrecidas": referentes_movil_ofrecidas,
+        "horas_operacion": horas_operacion,
+        "nivel_de_servicio": nivel_de_servicio,
+        "promedio_tiempo_operacion_segundos": promedio_tiempo_operacion_segundos,}
+
+def calc_roaming(df):
+    mensajes_entrantes = df["cantidad_de_mensajes_entrantes"]
+    mensajes_salientes = df["cantidad_de_mensajes_salientes"]
+    total_mensajes = mensajes_entrantes + mensajes_salientes
+    promedio_mensajes_por_interaccion = df["promedio_de_mensajes_por_interaccion"]
+
+    return {
+        "mensajes_entrantes": mensajes_entrantes,
+        "mensajes_salientes": mensajes_salientes,
+        "total_mensajes": total_mensajes,
+        "promedio_mensajes_por_interaccion": promedio_mensajes_por_interaccion,
+    }
+    
+def calc_congestion(df):
+    df_seis_uno = df["661_(%)"].sum()
+    df_uno_dos_uno = df["121_(%)"].sum()
+    
+    return {
+        "congestion_6611": int(df_seis_uno)
+    }
+
+def calc_automatismo(df):
+    total_correcto = df["total_correcto"].sum()
+    total_error = df["total_error"].sum()
+
+    porcentaje_correcto = ((total_correcto * 100 )/ (total_correcto + total_error)) if (total_correcto + total_error) > 0 else 0
+    porcentaje_error = ((total_error * 100) / (total_correcto + total_error)) if (total_correcto + total_error) > 0 else 0
+    total_automatismos = total_correcto + total_error
+
+    return {
+        "total_correcto": total_correcto,
+        "total_error": total_error,
+        "total_automatismos": total_automatismos,
+        "porcentaje_correcto": round(porcentaje_correcto, 2),
+        "porcentaje_error": round(porcentaje_error, 2),
+    }
+
+
+def calc_habilidad(df):
+    total_llamadas = df["ofrecidas"].sum()
+    atendidas = df["atendidas"].sum()
+    abandonadas = df["abandonadas"].sum()
+    dias_mes = df["fecha"].nunique()
+
+    porcentaje_no_atendidas = (abandonadas / total_llamadas) * 100 if total_llamadas else 0
+    indice_respuesta = 100 - porcentaje_no_atendidas
+    promedio_llamadas = total_llamadas / dias_mes if dias_mes else 0
+
+    return {
+        "llamadas_al_servicio": total_llamadas,
+        "llamadas_atendidas": atendidas,
+        "llamadas_abandonadas": abandonadas,
+        "porcentaje_no_atendidas": round(porcentaje_no_atendidas, 2),
+        "indice_respuesta": round(indice_respuesta, 2),
+        "promedio_llamadas_por_dia": math.trunc(promedio_llamadas),
+    }
+
+def ms_to_hms(ms):
+    seconds = int(ms / 1000)
+    return str(timedelta(seconds=seconds))
+
+def calc_incidencias(df):
+    df["responsabilidad"] = df["responsabilidad"].str.lower()
+    df = df[df["responsabilidad"] == "cliente"]
+    
+    fecha_incidente = df["fecha"]
+    descripcion_incidente = df["descripcion"]
+    
+    return {
+        "fecha_incidente" : fecha_incidente,
+        "descripcion_incidente" : descripcion_incidente
+    }
+
+def calc_reclamos(df):
+    df["_acumulado_o_detallado_"] = df["_acumulado_o_detallado_"].str.lower()
+    df = df[df["_acumulado_o_detallado_"] == "detallado"]
+
+    total_tiempo_llamadas = df["_manejo_total_"].sum()
+    total_llamadas = df["_manejo_"].sum()
+    nombre_de_cola = df["_nombre_de_cola_"].to_list()
+    nombre_de_codigo_de_conclusion = df["_nombre_de_codigo_de_conclusion_"].to_list()
+
+    return {
+        "total_tiempo_llamadas": ms_to_hms(total_tiempo_llamadas),
+        "total_llamadas": total_llamadas,
+        "nombre_de_cola": nombre_de_cola,
+        "nombre_de_codigo_de_conclusion": nombre_de_codigo_de_conclusion,
+    }
 
 
 if __name__ == "__main__":
